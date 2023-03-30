f88de5786b994ed5b447c61d115c703464da2b59
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-03-30 21:59:26 +0530

    ctalaogue port

--- vars.tf
+++ vars.tf
@@ -2,3 +2,10 @@ variable "env" {}
 variable "tags" {}
 variable "subnet_ids" {}
 variable "instance_type" {}
+variable "vpc_id" {}
+variable "allow_subnets" {}
+variable "bastion_cidr" {}
+variable "dns_domain" {}
+variable "component" {
+  default = "rabbitmq"
+}
