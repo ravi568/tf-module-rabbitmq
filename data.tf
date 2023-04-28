59557d5c85c72719a7e3238e4da03c1d4ac775e5
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-04-28 16:29:51 +0530

    shipping docker image

--- data.tf
+++ data.tf
@@ -1,8 +1,16 @@
+#data "aws_ami" "ami" {
+#  most_recent = true
+#  name_regex  = "devops-practice-with-ansible"
+#  owners      = ["self"]
+#}
+
+
 data "aws_ami" "ami" {
-  most_recent = true
-  name_regex  = "devops-practice-with-ansible"
-  owners      = ["self"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
 }
+
 
 data aws_route53_zone "domain"{
   name= var.dns_domain
--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+labauto ansible
 ansible-pull -i localhost, -U https://github.com/ravi568/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
